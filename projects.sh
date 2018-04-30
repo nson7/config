866bfa1969546dc29449bc0eed27ba4d770dca06
debian--nson7 <nson7@sina.com>
2018-04-30 23:17:15 +0800

    add projects.sh 2018.4.30

---
+++ projects.sh
@@ -0,0 +1,2 @@
+java -jar /home/nson7/opengrok-1.1-rc23/lib/opengrok.jar -P -S -v -s /home/nson7/opengrok-1.1-rc23/projects -d /home/nson7/opengrok-1.1-rc23/data -W /home/nson7/opengrok-1.1-rc23/data/configuration.xml
+
